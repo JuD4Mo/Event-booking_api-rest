63ff3019309e9bfbedcf0c847812cd4da88b3288
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2025-12-05 01:13:04 -0500

    Implement GET /ping endpoint to return all events and add event creation functionality

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"example.com/event-booking/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,15 +14,34 @@ func main() {
 
 	// Define a simple GET endpoint
 	router.GET("/ping", func(c *gin.Context) {
+		events := models.GetAllEvents()
 		// Return JSON response
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		c.JSON(http.StatusOK, events)
 	})
+
+	router.POST("/events", createEvent)
 
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
+}
+
+func createEvent(c *gin.Context) {
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		return
+	}
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Event created",
+		"event":   event,
+	})
 }
--- models/event.go
+++ models/event.go
@@ -4,9 +4,20 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserId      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	//TODO: store in db
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
